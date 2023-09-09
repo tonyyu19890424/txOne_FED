b83e897edcac2ce563b916131c484e6069ee70e5
Tony Yu <Tony_Yu@wistron.com>
2023-09-09 14:23:28 +0800

    Update files

--- src/MenuBtn.jsx
+++ src/MenuBtn.jsx
@@ -21,41 +21,41 @@ function MenuBtn() {
     setMenuOpen(!isMenuOpen);
   };
 
-  // const calculateModalPosition = () => {
-  //   if (buttonRef.current && isMenuOpen) {
-  //     const buttonRect = buttonRef.current.getBoundingClientRect();
-  //     setModalPosition({
-  //       top: buttonRect.bottom + window.scrollY,
-  //       left: buttonRect.left + window.scrollX,
-  //     });
-  //   }
-  // };
+  const calculateModalPosition = () => {
+    if (buttonRef.current && isMenuOpen) {
+      const buttonRect = buttonRef.current.getBoundingClientRect();
+      setModalPosition({
+        top: buttonRect.bottom + window.scrollY,
+        left: buttonRect.left + window.scrollX,
+      });
+    }
+  };
 
-  // const handleResize = () => {
-  //   cancelAnimationFrame(resizeTimeout);
-  //   resizeTimeout = requestAnimationFrame(() => {
-  //     calculateModalPosition();
-  //     setMenuOpen(false);
-  //   });
-  // };
+  const handleResize = () => {
+    cancelAnimationFrame(resizeTimeout);
+    resizeTimeout = requestAnimationFrame(() => {
+      calculateModalPosition();
+      setMenuOpen(false);
+    });
+  };
 
-  // useEffect(() => {
-  //   window.addEventListener("resize", handleResize);
-  //   return () => {
-  //     window.removeEventListener("resize", handleResize);
-  //   };
-  // }, []);
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
-  // useEffect(() => {
-  //   calculateModalPosition();
-  // }, [isMenuOpen]);
+  useEffect(() => {
+    calculateModalPosition();
+  }, [isMenuOpen]);
 
   return (
     <div>
       <Button
         onClick={toggleMenu}
-        style={{ marginTop: "5px", color: "white", backgroundColor: "#3d3d3d" }}
-        // ref={buttonRef}
+        style={{ marginTop: "5px", color: "white", backgroundColor: "#000000" }}
+        ref={buttonRef}
         data-testid="menu-btn"
       >
         <Icon style={{ color: "white", fontSize: "10px" }} icon="user" />
@@ -69,8 +69,8 @@ function MenuBtn() {
           isOpen={isMenuOpen}
           style={{
             position: "absolute",
-            // top: `${modalPosition.top}px`,
-            // left: `${modalPosition.left}px`,
+            top: `${modalPosition.top}px`,
+            left: `${modalPosition.left}px`,
             backgroundColor: "#3f3f3f",
             width: "200px",
             height: "300px",
