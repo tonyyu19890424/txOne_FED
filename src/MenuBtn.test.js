b190887fc39113b4d24f2f7a4710667925c45650
Tony Yu <Tony_Yu@wistron.com>
2023-09-09 13:53:18 +0800

    更新環境設定檔

--- src/MenuBtn.test.js
+++ src/MenuBtn.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable jest/no-conditional-expect */
 /* eslint-disable testing-library/no-wait-for-multiple-assertions */
 /* eslint-disable testing-library/no-unnecessary-act */
 /* eslint-disable testing-library/no-debugging-utils */
@@ -10,22 +11,26 @@ import { ColorModeProvider } from '@tonic-ui/react';
 import '@testing-library/jest-dom/extend-expect';
 import MenuBtn from './MenuBtn';
 
-test('MenuBtn 正確打開和關閉Modal', () => {
+test('MenuBtn 正確打開和關閉Modal', async () => {
   render(
     <ColorModeProvider>
       <MenuBtn />
     </ColorModeProvider>
   );
-  
+
   // 初始情況下，Modal應該是關閉的
   expect(screen.queryByTestId('modal')).toBeNull();
+  await waitFor(() => {
+    const openMenuButton = screen.queryByTestId('menu-btn');
+    expect(openMenuButton).toBeInTheDocument();
+  });
   const openMenuButton = screen.queryByTestId('menu-btn');
 
   // 點擊按鈕應該打開Modal
   act(() => {
-    fireEvent.mouseUp(openMenuButton);
+    fireEvent.click(openMenuButton);
   });
-  
+
   // 使用 waitFor 等待Modal出现
   waitFor(() => {
     // 確認Menu裡面的選項是否皆有正常出現
@@ -34,14 +39,117 @@ test('MenuBtn 正確打開和關閉Modal', () => {
     expect(screen.getByText('Privacy control')).toBeInTheDocument();
     expect(screen.queryByTestId('modal')).toBeInTheDocument();
   });
-  
+
   // 再次點擊按鈕應該關閉Modal
   act(() => {
-    fireEvent.mouseUp(openMenuButton);
+    fireEvent.click(openMenuButton);
   });
-  
+
   // 使用 waitFor 等待Modal消失
   waitFor(() => {
     expect(screen.queryByTestId('modal')).toBeNull();
+  });
+});
+
+test('toggleMenu should toggle isMenuOpen state', () => {
+  render(
+    <ColorModeProvider>
+      <MenuBtn />
+    </ColorModeProvider>
+  );
+  const openMenuButton = screen.queryByTestId('menu-btn');
+
+  act(() => {
+    fireEvent.mouseDown(openMenuButton);
+  });
+  waitFor(() => {
+    expect(openMenuButton).toHaveTextContent('Close Menu');
+  });
+
+  act(() => {
+    fireEvent.mouseDown(openMenuButton);
+  });
+  waitFor(() => {
+    expect(openMenuButton).toHaveTextContent('Open Menu');
+  });
+});
+
+test('closeMenu should set isMenuOpen to false', () => {
+  render(
+    <ColorModeProvider>
+      <MenuBtn />
+    </ColorModeProvider>
+  );
+  const openMenuButton = screen.queryByTestId('menu-btn');
+
+  act(() => {
+    fireEvent.mouseDown(openMenuButton);
+  });
+  waitFor(() => {
+    expect(openMenuButton).toHaveTextContent('Close Menu');
+  });
+  act(() => {
+    fireEvent.mouseDown(openMenuButton);
+  });
+  waitFor(() => {
+    expect(openMenuButton).toHaveTextContent('Open Menu');
+  });
+});
+
+test('calculateModalPosition should update modalPosition', () => {
+  render(
+    <ColorModeProvider>
+      <MenuBtn />
+    </ColorModeProvider>
+  );
+  const openMenuButton = screen.queryByTestId('menu-btn');
+  act(() => {
+    fireEvent.mouseDown(openMenuButton);
+  });
+  waitFor(() => {
+    const modal = screen.queryByTestId('modal');
+    const buttonRect = openMenuButton.getBoundingClientRect();
+    const modalPosition = modal.getBoundingClientRect();
+
+    expect(modalPosition.top).toBe(buttonRect.bottom + window.scrollY);
+    expect(modalPosition.left).toBe(buttonRect.left + window.scrollX);
+  });
+});
+
+test('handleResize should update modalPosition and close menu', () => {
+  render(
+    <ColorModeProvider>
+      <MenuBtn />
+    </ColorModeProvider>
+  );
+  const openMenuButton = screen.queryByTestId('menu-btn');
+  act(() => {
+    fireEvent.mouseDown(openMenuButton);
+  });
+
+  waitFor(() => {
+    const modal = screen.queryByTestId('modal');
+    const buttonRect = openMenuButton.getBoundingClientRect();
+    const modalPositionBeforeResize = modal ? modal.getBoundingClientRect() : null;
+
+    // 檢查Modal是否存在
+    expect(modalPositionBeforeResize).not.toBeNull();
+
+    // 模擬window大小變化
+    window.dispatchEvent(new Event('resize'));
+
+    // 使用 waitFor 等待Modal關閉
+    waitFor(() => {
+      const modalPositionAfterResize = modal ? modal.getBoundingClientRect() : null;
+
+      // 檢查Modal是否關閉了
+      expect(modalPositionAfterResize).toBeNull();
+
+      if (modalPositionBeforeResize) {
+        // 檢查 modalPosition 是否被更新
+        expect(modalPositionAfterResize.top).toBe(buttonRect.bottom + window.scrollY);
+        expect(modalPositionAfterResize.left).toBe(buttonRect.left + window.scrollX);
+      }
+    });
   });
 });
--- webpack.config.js
+++ webpack.config.js
@@ -3,31 +3,31 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js', // 入口文件
+  entry: './src/index.js', // 程式碼的入口檔案名稱
   mode: 'production',
   output: {
-    path: path.resolve(__dirname, 'build'), // 输出目录
-    filename: 'bundle.js', // 输出文件名
+    path: path.resolve(__dirname, 'build'), // 目的地資料夾
+    filename: 'bundle.js', // 輸出的文件名稱
   },
   performance: {
-    maxAssetSize: 1024 * 1024, // 1024 KiB
+    maxAssetSize: 1024 * 1024, // 1024 KB
     maxEntrypointSize: 1024*1024,
   },
   module: {
     rules: [
       {
         test: /\.js$/, // 匹配 JavaScript 文件
-        exclude: /node_modules/, // 排除 node_modules 目录
-        use: 'babel-loader', // 使用 Babel 进行转译
+        exclude: /node_modules/, // 排除 node_modules
+        use: 'babel-loader', // 使用 Babel 轉譯
       },
       {
         test: /\.jsx$/, // 匹配 JSX 文件
-        exclude: /node_modules/, // 排除 node_modules 目录
-        use: 'babel-loader', // 使用 Babel 进行转译
+        exclude: /node_modules/, // 排除 node_modules
+        use: 'babel-loader', // 使用 Babel 轉譯
       },
       {
         test: /\.css$/, // 匹配 CSS 文件
-        use: ['style-loader', 'css-loader'], // 使用 style-loader 和 css-loader 处理 CSS
+        use: ['style-loader', 'css-loader'], // 使用 style-loader 和 css-loader 處理 CSS
       },
     ],
   },
